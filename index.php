92ef1df4d00cf41e8366f99a420e46fb521ec0bf
Troy <troy.stilting@st-2.com>
2023-09-27 13:57:03 +0200

    basic html homepage made

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- responsive code -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- external links -->
+    <link rel="stylesheet" href="css.css">
+    <title>Nerdy Gadgets</title>
+</head>
+<header>
+    <!-- het logo -->
+    <div class="logo">
+    <img src="" alt="Nerdy Gadgets Logo">
+    </div>
+
+    <!-- de search bar -->
+    <div class="search">
+
+    </div>
+
+    <!-- displayt de shopping cart -->
+    <div class="de legend">
+        <p>	&#128722 </p>
+    </div>
+
+    <!-- profile en login -->
+    <div class="profile">
+        <img src="" alt="Profile Picture">
+    </div>
+</header>
+<nav>
+    <!-- vul hier uw pagina's in voor toekomstige sprints -->
+    <a href="#">sample</a>
+</nav>
+<body>
+    <!-- als je wil kan je hier bijvoorbeeld de slogan plakken als je wilt -->
+    <div class="slogan">
+
+    </div>
+    <!-- aanbevolen producten slider -->
+    <div class="aanbevolen">
+        <!-- plak uw slider hier -->
+    </div>
+</body>
+<footer>
+    <!-- indien nodig? -->
+</footer>
+</html>
